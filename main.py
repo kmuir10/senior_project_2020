d7af06408c5666a6fd99373c6fb1bf8dcfc3cb82
kmuir10 <kylemuir567@gmail.com>
2020-03-04 14:29:41 -0800

    Improved readability, initialized W better. Violin can now be reconstructed perfectly

--- main.py
+++ main.py
@@ -23,25 +23,24 @@ def stft(vx, vfs):
     vx_copy = np.pad(vx_copy, (0, sample_size - vx_copy.shape[0] % sample_size), constant_values=(0,))
     vx_copy = np.split(vx_copy, vx_copy.shape[0] // sample_size)
 
-    stft = np.apply_along_axis(np.fft.rfft, 1, vx_copy)
+    stft = np.apply_along_axis(np.fft.rfft, 1, vx_copy).T
     return np.abs(stft), np.angle(stft)
 
 
 def nmf(V, k=63):
-    t = V.shape[0]
-    f = V.shape[1]
-    W = np.ones((k, f))
-    H = np.ones((t, k))
+    t = V.shape[1]
+    f = V.shape[0]
+    W = np.random.randint(np.min(V), np.max(V), (f, k))
+    H = np.ones((k, t))
     v1 = np.ones(V.shape)
     print('f = {0}, t = {1}, W: {2}, H: {3}, v1: {4}'.format(f, t, W.shape, H.shape, v1.shape))
-    print(np.transpose(W).shape, V.shape, np.dot(H, W).shape)
 
-    for i in range(0,100):
+    for i in range(0, 100):
         print('nmf loop', i)
-        # H = H * np.dot(np.transpose(W), V / np.dot(W, H)) / np.dot(np.transpose(W), v1)
-        # W = W * np.dot(V / np.dot(W, H), np.transpose(H)) / np.dot(v1, np.transpose(H))
-        H = H * np.dot(V / np.dot(H, W), np.transpose(W)) / np.dot(v1, np.transpose(W))
-        W = W * np.dot(np.transpose(H), V / np.dot(H, W)) / np.dot(np.transpose(H), v1)
+        H = H * np.dot(W.T, V / np.dot(W, H)) / np.dot(W.T, v1)
+        W = W * np.dot(V / np.dot(W, H), H.T) / np.dot(v1, H.T)
+        # H = H * np.dot(V / np.dot(H, W), np.transpose(W)) / np.dot(v1, np.transpose(W))
+        # W = W * np.dot(np.transpose(H), V / np.dot(H, W)) / np.dot(np.transpose(H), v1)
 
     return W, H
 
@@ -60,10 +59,10 @@ def main():
     # basis_partial, activation_partial = nmf(stft_mag[:35], 200)
     # print('partial nmf done', basis_partial.shape, activation_partial.shape, stft_mag.shape)
 
-    basis, activation = nmf(stft_mag, stft_mag.shape[0])
+    basis, activation = nmf(stft_mag, stft_mag.shape[1])
     print('main nmf done')
 
-    reconstructed = np.concatenate(istft(np.dot(activation, basis), stft_phase)).ravel().astype(vx.dtype)
+    reconstructed = np.concatenate(istft(np.dot(basis, activation).T, stft_phase.T)).ravel().astype(vx.dtype)
     print('reconstruction done')
 
     wavfile.write("reconstructed_violin.wav", vfs, reconstructed)
